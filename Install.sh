1c72dc9758fb8dcdd78ce560e0bc5ce72f1930d8
glubix <merlin.bourge@gmail.com>
2023-06-05 12:01:50 +0200

    Ajout compatibilité linux && ajout installation sans Sudo

--- Install.sh
+++ Install.sh
@@ -4,7 +4,7 @@
 script_path="overlay.sh"
 
 # Répertoire où sera installé le script
-install_dir="/usr/local/bin"
+install_dir="$HOME/bin"
 
 # Nom du lien symbolique pour exécuter le script
 symlink_name="overlinette"
@@ -15,36 +15,29 @@ if [ ! -f "$script_path" ]; then
     exit 1
 fi
 
-# Vérifier si le répertoire d'installation existe
+# Vérifier si le répertoire d'installation existe, sinon le créer
 if [ ! -d "$install_dir" ]; then
-    echo "Erreur : Le répertoire d'installation $install_dir n'existe pas."
-    exit 1
+    mkdir -p "$install_dir"
 fi
 
 # Copier le script vers le répertoire d'installation
 cp "$script_path" "$install_dir"
 
-# Créer le lien symbolique dans /usr/local/bin
+# Créer le lien symbolique dans le répertoire local
 ln -s "$install_dir/$(basename "$script_path")" "$install_dir/$symlink_name"
 
 # Ajouter le fichier de manuel
-man_dir="/usr/local/share/man/man1"
+man_dir="$HOME/man/man1"
 man_filename="overlinette.1"
 
-# Vérifier si le répertoire des fichiers de manuel existe
+# Vérifier si le répertoire des fichiers de manuel existe, sinon le créer
 if [ ! -d "$man_dir" ]; then
-    echo "Erreur : Le répertoire des fichiers de manuel $man_dir n'existe pas."
-    exit 1
+    mkdir -p "$man_dir"
 fi
 
 # Copier le fichier de manuel vers le répertoire des fichiers de manuel
 cp "$man_filename" "$man_dir"
 
-# Vérifier le système d'exploitation et ajuster les messages en conséquence
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Le script a été installé avec succès sur macOS."
-    echo "Vous pouvez maintenant exécuter le script en utilisant la commande 'overlinette'."
-else
-    echo "Le script a été installé avec succès sur Linux."
-    echo "Vous pouvez maintenant exécuter le script en utilisant la commande 'overlinette'."
-fi
+# Afficher un message d'installation réussie
+echo "Le script a été installé avec succès."
+echo "Vous pouvez maintenant exécuter le script en utilisant la commande '$symlink_name'."
